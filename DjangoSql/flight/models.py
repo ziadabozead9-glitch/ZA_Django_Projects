b48c2fd93a64a586a7f4bf0e3b92f1f55248cdbd
ziadabozead9-glitch <ziadabozead9@gmail.com>
2026-06-21 03:19:45 +0800

    Add Airport model, update Flight FKs, templates
    Introduce airport model (unique code and city) and switch flight.origin/destination from CharFields to ForeignKeys with related_names. Add migrations (0002, 0003) to reflect these schema changes and update model __str__ implementations. Add basic templates (layout.html, index.html), stylesheet (static/flight/styles.css), and wire up index view and URL to render all flights. Remove obsolete addons script and include updated DB and compiled files generated by the changes.

--- DjangoSql/flight/models.py
+++ DjangoSql/flight/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class airport (models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    city = models.CharField(max_length=64, unique=True)
+
+    def __str__(self) :
+        return f" {self.code} {self.city}"
+
 class flight (models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(airport, on_delete = models.CASCADE, related_name="departers")
+    destination = models.ForeignKey(airport, on_delete = models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.id} ({self.origin}) ({self.destination})({self.duration})"
--- DjangoSql/flight/views.py
+++ DjangoSql/flight/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import flight
 # Create your views here.
+
+def index(request):
+    return render(request, "flight/index.html", {
+        "flight":flight.objects.all()
+        })
